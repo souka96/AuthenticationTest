fb05d3cc3035a17f541adcc92cae8026107470ec
souka96 <soukaina.zouyne34@gmail.com>
2018-07-14 17:20:25 +0000

    Initial commit

---
+++ app/src/main/java/com/ezzouyn/soukaina/authenticationtest/Utils.java
@@ -0,0 +1,11 @@
+package com.ezzouyn.soukaina.authenticationtest;
+
+/**
+ * Created by soukaina on 14/07/2018.
+ */
+
+public class Utils {
+    public static final String BASE_IP          = "http://10.43.182.254/";
+    public static final String LOGIN_URL        = BASE_IP + "ftthauth/login.php";
+    public static final String REGISTER_URL     = BASE_IP + "ftthauth/register.php";
+}
